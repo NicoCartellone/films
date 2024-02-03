6f2f5cecaa3c08e56f7b75d0a6489f63991edf30
Nico <nico_cartellone@hotmail.com>
2024-02-03 03:45:45 -0300

    fix: styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <header className="body-font">
       <div className="flex justify-center p-5">
-        <a className="flex items-center order-first mb-4 font-medium text-black dark:text-white lg:order-none lg:w-1/5 title-font lg:items-center lg:justify-center md:mb-0">
+        <a className="flex items-start order-first mb-4 font-medium text-black dark:text-white lg:order-none lg:w-1/5 title-font lg:items-center lg:justify-center md:mb-0">
           <span className="text-4xl xl:block lg:hidden">🍿Films</span>
         </a>
       </div>
